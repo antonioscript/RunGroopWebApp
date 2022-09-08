b31a7b7eaf727a4396805e48b0f6ee64d74400c2
Antônio Rocha <antoniojunior159@gmail.com>
2022-09-07 23:54:03 -0300

    update

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RunGroopWebApp.Models;
 
 namespace RunGroopWebApp.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
