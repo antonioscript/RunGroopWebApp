7eea03601afd2133c266a933ce2f0802fcb41d71
Antônio Rocha <antoniojunior159@gmail.com>
2022-09-07 23:39:52 -0300

    Update

--- Data/Seed.cs
+++ Data/Seed.cs
@@ -125,14 +125,14 @@ namespace RunGroopWebApp.Data
 
         //         //Users
         //         var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
-        //         string adminUserEmail = "teddysmithdeveloper@gmail.com";
+        //         string adminUserEmail = "antonioadm@gmail.com";
 
         //         var adminUser = await userManager.FindByEmailAsync(adminUserEmail);
         //         if (adminUser == null)
         //         {
         //             var newAdminUser = new AppUser()
         //             {
-        //                 UserName = "teddysmithdev",
+        //                 UserName = "antonioadm",
         //                 Email = adminUserEmail,
         //                 EmailConfirmed = true,
         //                 Address = new Address()
